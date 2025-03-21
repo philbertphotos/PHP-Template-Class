f39cf0cfd16dd443fe2d953cb8a6ed11385a135e
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2025-03-21 23:59:26 +0300

    Update Template.php
    endfor issue fixed.

--- Template.php
+++ Template.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Template Class
+ *
+ * @category  Template
+ * @package   Template
+ * @author    Osman Cakmak <info@oxcakmak.com>
+ * @copyright Copyright (c) 2024-?
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      https://github.com/oxcakmak/PHP-Template-Class
+ * @version   1.1.0
+ */
 class Template {
     private $variables = [];
     private $templateDir;
@@ -47,7 +58,7 @@ class Template {
         // Process nested for loops (from innermost to outermost)
         while ($iteration < $maxIterations) {
             // Find all for loops in the template
-            if (!preg_match_all('/\{%\s*for\s+(\w+)\s+in\s+([a-zA-Z0-9._]+)\s*%\}(.*)\{%\s*endfor\s*%\}/s', $template, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
+            if (!preg_match_all('/\{%\s*for\s+(\w+)\s+in\s+([a-zA-Z0-9._]+)\s*%\}(.*?)\{%\s*endfor\s*%\}/s', $template, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
                 break;
             }
     
